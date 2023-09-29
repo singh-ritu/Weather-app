6023c5a920c12c84fba3c72ad30a76a7a93d2645
Ritu <singhritu241101@gmail.com>
2023-09-29 13:06:33 +0530

    Try and catch handled

--- weather.js
+++ weather.js
@@ -1,61 +1,76 @@
-// http://api.weatherapi.com/v1/current.json?key=3bd2bd0b2f68434a8ed82333232809&q=London&aqi=no
+let temp_f = "";
+let temp_c = "";
+let isCurrentlyCelcius = true;
 
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
     const apiString =
       position.coords.latitude + "," + position.coords.longitude;
     getWeather(apiString);
   });
 }
-let temp_f = "";
 async function getWeather(str = "") {
-  const country = document.getElementsByClassName("input")[0].value;
-  const response = await fetch(
-    ` http://api.weatherapi.com/v1/current.json?key=3bd2bd0b2f68434a8ed82333232809&q=${
-      str ? str : country
-    }&aqi=no`
-  );
-  const data = await response.json();
-  temp_f = data.current.temp_f;
+  try {
+    const country = document.getElementsByClassName("input")[0].value;
+    const response = await fetch(
+      ` http://api.weatherapi.com/v1/current.json?key=3bd2bd0b2f68434a8ed82333232809&q=${
+        str ? str : country
+      }&aqi=no`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to Fetch the data");
+    }
+    const data = await response.json();
+    temp_f = data.current.temp_f;
+    temp_c = data.current.temp_c;
 
-  const temperature = document.querySelector("#temperature");
-  temperature.innerText = data.current.temp_c;
+    const temperature = document.querySelector("#temperature");
+    temperature.innerText = data.current.temp_c;
 
-  const windSpeed = document.querySelector("#wind-speed");
-  windSpeed.innerText = data.current.wind_kph;
+    const windSpeed = document.querySelector("#wind-speed");
+    windSpeed.innerText = data.current.wind_kph;
 
-  const humidity = document.querySelector("#humidity");
-  humidity.innerText = data.current.humidity;
+    const humidity = document.querySelector("#humidity");
+    humidity.innerText = data.current.humidity;
 
-  const name = document.querySelector("#name");
-  name.innerText = data.location.name;
+    const name = document.querySelector("#name");
+    name.innerText = data.location.name;
 
-  if (!document.querySelector("#weather-img")) {
-    const weatherStatusImg = document.createElement("img");
-    const weatherStatus = document.createElement("h3");
+    if (!document.querySelector("#weather-img")) {
+      const weatherStatusImg = document.createElement("img");
+      const weatherStatus = document.createElement("h3");
 
-    weatherStatus.id = "weather-status";
-    weatherStatusImg.id = "weather-img";
+      weatherStatus.id = "weather-status";
+      weatherStatusImg.id = "weather-img";
 
-    weatherStatusImg.src = data.current.condition.icon;
-    weatherStatus.innerText = data.current.condition.text;
+      weatherStatusImg.src = data.current.condition.icon;
+      weatherStatus.innerText = data.current.condition.text;
 
-    const tempContainer = document.querySelector(".temp-container");
+      const tempContainer = document.querySelector(".temp-container");
 
-    tempContainer.append(weatherStatusImg, weatherStatus);
-  } else {
-    const weatherStatusImg = document.querySelector("#weather-img");
-    const weatherStatus = document.querySelector("#weather-status");
+      tempContainer.append(weatherStatusImg, weatherStatus);
+    } else {
+      const weatherStatusImg = document.querySelector("#weather-img");
+      const weatherStatus = document.querySelector("#weather-status");
 
-    weatherStatusImg.src = data.current.condition.icon;
-    weatherStatus.innerText = data.current.condition.text;
+      weatherStatusImg.src = data.current.condition.icon;
+      weatherStatus.innerText = data.current.condition.text;
+    }
+  } catch (error) {
+    console.log("An error occurred:", error);
   }
-
-  console.log(country, data);
 }
 
 function changeUnit() {
   const temperature = document.querySelector("#temperature");
-  temperature.innerText = temp_f;
+  const tempUnit = document.querySelector("#temp-unit");
+  if (isCurrentlyCelcius) {
+    temperature.innerText = temp_f;
+    isCurrentlyCelcius = false;
+    tempUnit.innerText = "°F";
+  } else {
+    temperature.innerText = temp_c;
+    isCurrentlyCelcius = true;
+    tempUnit.innerText = "°C";
+  }
 }
